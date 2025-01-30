6a01fcb4f917aec46abb193bfc438eb54965703d
sankalana960 <sankalana960@gmail.com>
2025-01-30 18:36:00 +0530

    final today

--- src/getuser/getuser.controller.ts
+++ src/getuser/getuser.controller.ts
@@ -1,15 +1,14 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { GetUserService } from "./getuser.service";
-import { API_ROUTES } from "src/common/routes";
 
 
-@Controller(API_ROUTES.USER.BASE)
+@Controller()
 export class GetUserController{
 
     constructor(public readonly getuserserveice: GetUserService){}
 
 
-    @Get(API_ROUTES.USER.GETUSER)
+    @Get("getusers/:id")
     getUser(@Param('id') userId: string){
         const parsedUserId = parseInt(userId)
         return this.getuserserveice.getUser(parsedUserId)
